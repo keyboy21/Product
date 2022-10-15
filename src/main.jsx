efeb6290a5203fc9dd1133aafb3ddd76f0b8f01f
Yusuf <yusufsaydullayev@gmail.com>
2022-10-15 18:15:14 +0500

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.css'
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import 'antd/dist/antd.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
